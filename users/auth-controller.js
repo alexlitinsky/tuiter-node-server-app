629afaef642d43d1cfd8d0cfbb931a62d8df091c
alexlitinsky <litinsky.a@northeastern.edu>
2023-06-13 16:25:27 -0700

    going back to new profile

--- users/auth-controller.js
+++ users/auth-controller.js
@@ -30,28 +30,28 @@ const AuthController = (app) => {
   };
 
   const profile = async (req, res) => {
-    // working on local server
+    // working on local server (ig not anymore??)
 
-    console.log("all the users in profile method", usersDao.findAllUsers());
-    console.log(new Date());
-    console.log("req session id in profile: ", req.session._id);
-    const currentUser = req.session["currentUser"];
-    console.log("profile current user", currentUser);
-    if (!currentUser) {
-      res.sendStatus(404);
-      return;
-    }
-    res.json(currentUser);
-
-    // loads up profile correctly but doens't create new users or updates them
-    // const session = req.session["currentUser"];
-    // const userId = session._id;
-    // const currentUser = await usersDao.findUserById(userId);
+    // console.log("all the users in profile method", usersDao.findAllUsers());
+    // console.log(new Date());
+    // console.log("req session id in profile: ", req.session._id);
+    // const currentUser = req.session["currentUser"];
+    // console.log("profile current user", currentUser);
     // if (!currentUser) {
     //   res.sendStatus(404);
     //   return;
     // }
     // res.json(currentUser);
+
+    // loads up profile correctly but doens't create new users or updates them
+    const session = req.session["currentUser"];
+    const userId = session._id;
+    const currentUser = await usersDao.findUserById(userId);
+    if (!currentUser) {
+      res.sendStatus(404);
+      return;
+    }
+    res.json(currentUser);
   };
   const logout = async (req, res) => {
     req.session.destroy();
